8540c2f6c42c8f963e995dfbcaca3df11d463254
Kanawanagasaki <62866497+Kanawanagasaki@users.noreply.github.com>
2023-10-03 10:58:01 +0000

    Deploying to gh-pages from  @ 0cf6af0691cdd7a854718e2e5e23b51b44cff176 🚀

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const twitchChatListeners = [];
 const framesListeners = [];
+const audioMap = {};
 
 const client = new tmi.Client({
     channels: ["darkviperau"]
@@ -14,7 +15,8 @@ client.on('message', async (channel, tags, message, self) => {
             messageId: tags.id,
             userId: tags['user-id'],
             userLogin: tags.username,
-            userDisplayName: tags['display-name']
+            userDisplayName: tags['display-name'],
+            color: tags.color
         });
     }
 });
@@ -63,4 +65,27 @@ function framesRemoveListener(dotnetRef) {
     const index = framesListeners.indexOf(dotnetRef);
     if (0 <= index)
         framesListeners.splice(index, 1);
+}
+
+function playAudio(dotnetRef, path) {
+    if (!audioMap[path])
+    {
+        audioMap[path] = new Audio(path);
+        audioMap[path].volume = .5;
+        audioMap[path].addEventListener("ended", async () => {
+            await dotnetRef.invokeMethodAsync("OnAudioEnded", path);
+        });
+    }
+
+    audioMap[path].currentTime = 0;
+    audioMap[path].play();
+}
+
+async function stopAudio(dotnetRef, path) {
+    if (!audioMap[path])
+        return;
+
+    audioMap[path].pause();
+    audioMap[path].currentTime = 0;
+    await dotnetRef.invokeMethodAsync("OnAudioEnded", path);
 }
